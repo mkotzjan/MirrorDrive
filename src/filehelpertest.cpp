00d9e1c3dab15efad8e81b902e968d7d5db16329
ChaosBelcebub <mich3ael@web.de>
2016-04-21 18:41:02 +0200

    first tests

---
+++ src/filehelpertest.cpp
@@ -0,0 +1,5 @@
+#include "filehelpertest.h"
+
+FileHelperTest::FileHelperTest()
+{
+}
---
+++ src/filehelpertest.h
@@ -0,0 +1,10 @@
+#ifndef FILEHELPERTEST_H
+#define FILEHELPERTEST_H
+
+class FileHelperTest
+{
+public:
+    FileHelperTest();
+};
+
+#endif // FILEHELPERTEST_H
